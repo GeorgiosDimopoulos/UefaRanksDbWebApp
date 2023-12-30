4ff06af333de6826e011d1ebc789099dbfbcaec0
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2023-12-30 23:47:25 +0100

    Add Match ViewModel

---
+++ UefaRankingApplication.Core/ViewModels/MatchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using UefaRankingApplication.Data.Models;
+
+namespace UefaRankingApplication.Data.ViewModels
+{
+    public class MatchViewModel
+    {
+        public Match Match { get; set; }
+
+        public IEnumerable<SelectListItem> TeamsList { get; set; }
+    }
+}
