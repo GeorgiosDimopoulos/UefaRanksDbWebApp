702e35b5371c0ae564f832abb10c1e20f94afcab
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-01-02 20:04:07 +0100

    Add MatchTeamViewModel

---
+++ UefaRankingApplication.Core/ViewModels/MatchTeamViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using UefaRankingApplication.Data.Models;
+
+namespace UefaRankingApplication.Data.ViewModels
+{
+    public class MatchTeamViewModel
+    {
+        public int Id { get; set; }
+
+        public Team Team { get; set; }
+
+        public IEnumerable<SelectListItem> TeamsList { get; set; }
+    }
+}
