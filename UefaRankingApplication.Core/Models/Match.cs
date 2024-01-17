fafcb0db576ea854ffd224ee61310b5a4f4465e4
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-01-17 20:54:04 +0100

    Add Match entity

---
+++ UefaRankingApplication.Core/Models/Match.cs
@@ -0,0 +1,12 @@
+﻿namespace UefaRankingApplication.Data.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+        public string PlayingCup { get; set; }
+
+        public string Result { get; set; }
+    }
+}
