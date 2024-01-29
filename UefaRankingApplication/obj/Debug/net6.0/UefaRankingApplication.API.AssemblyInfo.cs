4f7213ea67e121489d905b1e54302b1ab9d7f801
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-01-29 15:56:17 +0100

    Add WPF Layer

--- UefaRankingApplication/obj/Debug/net6.0/UefaRankingApplication.API.AssemblyInfo.cs
+++ UefaRankingApplication/obj/Debug/net6.0/UefaRankingApplication.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UefaRankingApplication.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abf175d8ceea3560ec1d7697acb4d695c73acf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UefaRankingApplication.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UefaRankingApplication.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
